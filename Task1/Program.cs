f8654b63d6318afa846e4d353f253ac0cf7671b6
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-09-27 00:24:20 +0400

    Task2 added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,37 @@
 ﻿//Verilmiş 4 rəqəmli ədədin mərtəbələrinin cəmini (Məsələn: input 1234, output 10) tapan alqoritmi yazın.
 //Butun coxreqemliler ucun kecerlidir
 
+//Console.WriteLine("Ededi daxil edin:");
+
+//int num = int.Parse(Console.ReadLine());
+//int sum = 0;
+
+//while (num / 10 > 0)
+//{
+//    sum += num % 10;
+//    num/= 10;
+//}
+//Console.WriteLine(sum+num);
+
+
+
+
+
+
+//Task2
+//Verilmiş 3 rəqəmli ədədin bütün mərtəbələrinin bir-birindən fərqli olub olmadığını tapan alqoritmi yazın
+//(Məsələn: input 223, output "ferqli olmayan rəqəmlər var " (false) ).
+
+
 Console.WriteLine("Ededi daxil edin:");
 
 int num = int.Parse(Console.ReadLine());
-int sum = 0;
 
-while (num / 10 > 0)
+if (num / 100 < 10)
 {
-    sum += num % 10;
-    num/= 10;
+    if (num % 10 == (num / 10) % 10 || num % 10 == num / 100 || (num / 10) % 10 == num / 100)
+    {
+        Console.WriteLine("ferqli olmayan reqemler var");
+    }
+    else Console.WriteLine("ferqlidir");
 }
-Console.WriteLine(sum+num);
