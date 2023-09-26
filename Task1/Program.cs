2f999c7a936b1db1a5cfa3a9300072857510086f
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-09-26 23:41:30 +0400

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//Verilmiş 4 rəqəmli ədədin mərtəbələrinin cəmini (Məsələn: input 1234, output 10) tapan alqoritmi yazın.
+//Butun coxreqemliler ucun kecerlidir
+
+Console.WriteLine("Ededi daxil edin:");
+
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (num / 10 > 0)
+{
+    sum += num % 10;
+    num/= 10;
+}
+Console.WriteLine(sum+num);
